33a72097c7206e984656e407cfeaf4c13414d41a
sahil-sahu-dev <sahilsahucode@gmail.com>
2022-04-26 08:09:51 +0530

    basic ui working with dummy data

--- TweekLabsHack/TweekLabsHack/Models/AthleteModel.swift
+++ TweekLabsHack/TweekLabsHack/Models/AthleteModel.swift
@@ -10,17 +10,39 @@ import Foundation
 
 struct Athlete: Identifiable {
     
+    static var idGenerator: Int = 0
     var id: Int
     var name: String
     var bowler_type: String
     var athlete_sessions: [AthleteSession]
+    
+    init(name: String, bowler_type: String, athlete_sessions: [AthleteSession]) {
+        
+        self.id = Athlete.idGenerator
+        self.name = name
+        self.bowler_type = bowler_type
+        self.athlete_sessions = athlete_sessions
+        
+        Athlete.idGenerator += 1
+    }
+    
 }
 
 
 struct AthleteSession: Identifiable {
     
+    static var idGenerator: Int = 0
     var id: Int
     var balls_bowled: Int
     var date: Date // format = dd/mm/yyyy, mm:hh aa
+    
+    init(balls_bowled: Int, date: Date) {
+        
+        self.id = Athlete.idGenerator
+        self.balls_bowled = balls_bowled
+        self.date = date
+        
+        Athlete.idGenerator += 1
+    }
     
 }
--- TweekLabsHack/TweekLabsHack/TweekLabsHackApp.swift
+++ TweekLabsHack/TweekLabsHack/TweekLabsHackApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct TweekLabsHackApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            AthleteHomeView(athleteDoc: AthleteDocument())
         }
     }
 }
