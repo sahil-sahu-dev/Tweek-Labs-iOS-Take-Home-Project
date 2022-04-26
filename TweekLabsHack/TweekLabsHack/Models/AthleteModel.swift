2836fdd4c062d46a6a30d0610cac046912665433
sahil-sahu-dev <sahilsahucode@gmail.com>
2022-04-26 07:34:40 +0530

    Athlete + AthleteSession struct

--- TweekLabsHack/TweekLabsHack/Models/AthleteModel.swift
+++ TweekLabsHack/TweekLabsHack/Models/AthleteModel.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+
+struct Athlete: Identifiable {
+    
+    var id: Int
+    var name: String
+    var bowler_type: String
+    var athlete_sessions: [AthleteSession]
+}
+
+
+struct AthleteSession: Identifiable {
+    
+    var id: Int
+    var balls_bowled: Int
+    var date: Date // format = dd/mm/yyyy, mm:hh aa
+    
+}
