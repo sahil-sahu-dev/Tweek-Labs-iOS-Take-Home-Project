96dcbba1e075c6ab97bf22915584a01602c3ee58
sahil-sahu-dev <80636783+sahil-sahu-dev@users.noreply.github.com>
2022-04-26 07:20:16 +0530

    initial commit

---
+++ TweekLabsHack/TweekLabsHack/TweekLabsHackApp.swift
@@ -0,0 +1,17 @@
+//
+//  TweekLabsHackApp.swift
+//  TweekLabsHack
+//
+//  Created by Sahil Sahu on 26/04/22.
+//
+
+import SwiftUI
+
+@main
+struct TweekLabsHackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
